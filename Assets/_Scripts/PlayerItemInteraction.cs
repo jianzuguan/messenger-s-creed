6d7ca631b26bee35734ee55ff7ab80955a12beb6
Jianzu Guan <guan-1995@hotmail.com>
2017-03-30 17:07:16 +0100

    player can pick up item and add to inventory

---
+++ Assets/_Scripts/PlayerItemInteraction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerItemInteraction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionStay(Collision collision) {
+        if (collision.gameObject.tag == "Item" && Input.GetButtonDown("Interact")) {
+            gameObject.GetComponent<PlayerStats>().TakeItem(collision.gameObject);
+            collision.gameObject.transform.parent = gameObject.transform;
+            collision.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/_Scripts/PlayerStats.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+    public List<GameObject> inventory = new List<GameObject>();
+
+    public bool TakeItem(GameObject item) {
+        inventory.Add(item);
+        // TODO: check if the item is succesfully added
+        return true;
+    }
+
+    public bool DropItem(GameObject item) {
+        return inventory.Remove(item);
+    }
+}
