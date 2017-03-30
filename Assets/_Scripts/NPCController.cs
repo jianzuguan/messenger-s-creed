4a58043df8f27843a901ba2f9bea888da8c8a39d
Jianzu Guan <guan-1995@hotmail.com>
2017-03-30 16:28:52 +0100

    added npcController for display dialog

---
+++ Assets/_Scripts/NPCController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPCController : MonoBehaviour {
+
+    public GameObject canvasDialog;
+    public Text speech;
+
+    private NPCStats stats;
+        
+	// Use this for initialization
+	void Start () {
+        canvasDialog.SetActive(false);
+        stats = GetComponent<NPCStats>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == "Player") {
+            if (stats.needItem) {
+                speech.text = stats.dialogNeed;
+                canvasDialog.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/_Scripts/NPCStats.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCStats : MonoBehaviour {
+
+    public string npcName;
+    [Header("Dialogs")]
+    public string dialogNeed;
+    public string dialogSell;
+    public string dialogGive;
+    [Header("Quest status")]
+    public bool needItem;
+
+    [Header("Inventory")]
+    public GameObject[] inventory;
+}
